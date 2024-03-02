4258c897c79f0dad5804d8e60f66fffa95726f25
Gergő <46539838+gergo-szabo@users.noreply.github.com>
2024-03-02 04:05:44 +0100

    remove logs

--- extraction.py
+++ extraction.py
@@ -12,11 +12,31 @@ token_prompt = 'You are a helpful assistant. brief and precise. You are extracti
 
 def extract_token_info(text_chunks, embeddings, logger, max_retries=3):
     project_context = get_project_context(text_chunks, embeddings, prompt=token_prompt, top_k=10)
-    logger.info(f'chain extraction context: {project_context}')
+    #logger.info(f'chain extraction context: {project_context}')
 
     retries = 0
     while retries < max_retries:
         response = get_openai_completion(token_prompt + project_context, logger)
+        if response is None:
+            time.sleep(1)
+        else:
+            return response
+    return 'Extraction failed.'
+
+lunchpad_prompt = 'You are a helpful assistant. brief and precise. You are extracting information from scrapped crypto project websites.\n' \
+                  'Extract the name of the token and token symbol!\n' \
+                  'If no token or token symbol mentioned in the text then answer: No information found!\n' \
+                  'Answer format:\n' \
+                  'token name, token symbol'
+
+
+def extract_lunchpad_info(text_chunks, embeddings, logger, max_retries=3):
+    project_context = get_project_context(text_chunks, embeddings, prompt=lunchpad_prompt, top_k=10)
+    #logger.info(f'chain extraction context: {project_context}')
+
+    retries = 0
+    while retries < max_retries:
+        response = get_openai_completion(lunchpad_prompt + project_context, logger)
         if response is None:
             time.sleep(1)
         else:
