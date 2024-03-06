e4ec56ef3a9e9994b914e78b3cfcfd4d097f8db2
Gergő <46539838+gergo-szabo@users.noreply.github.com>
2024-03-06 03:17:18 +0100

    scrapping finetune

--- crawler.py
+++ crawler.py
@@ -62,6 +62,22 @@ def get_pdf_text(url: str):
         return ""
     return text
 
+def get_important_urls(url: str):
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        links = soup.find_all('a', href=True)
+        try:
+            twitter_link = [link['href'] for link in links if 'twitter.com' in link['href']][0]
+        except:
+            twitter_link = None
+        try:
+            telegram_link = [link['href'] for link in links if 't.me' in link['href']][0]
+        except:
+            telegram_link = None
+    except:
+        twitter_link = telegram_link = None
+    return twitter_link, telegram_link
 
 def crawl(url: str):
     # Collect page and document links related to the input URL
@@ -78,36 +94,25 @@ def crawl(url: str):
             document_url_list.extend(document_urls)
             level_1.extend([link for link in page_links if link not in visited])
 
-    """for current_url in level_1:
+    for current_url in level_1:
+        if "docs" not in current_url:
+            continue  # Scrape only the project technical documentation in 2 level depth
         if current_url not in visited:
             visited.add(current_url)
             page_links, document_urls = get_links(current_url)
             document_url_list.extend(document_urls)
             level_2.extend([link for link in page_links if link not in visited])
-        if len(level_2) > 100:
-            break"""
 
     # Parse pages
     url_list = set(level_0 + level_1 + level_2)
-    page_texts = [f"The following text is from {i}:\n\n{get_page_text(i)}" for i in url_list]
+    page_texts_raw = [(i, get_page_text(i)) for i in url_list]
+    page_texts = [f"The following text is from {i}:\n{text}" for i, text in page_texts_raw if len(text) > 0]
 
     # Parse pdf-s (e.g. whitepapers)
-    document_texts = [f"The following text is from {i}:\n\n{get_pdf_text(i)}" for i in document_url_list]
+    document_texts_raw = [(i, get_pdf_text(i)) for i in document_url_list]
+    document_texts = [f"The following text is from {i}:\n{text}" for i, text in document_texts_raw if len(text) > 0]
 
     # Twitter and telegram links
-    try:
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        links = soup.find_all('a', href=True)
-        try:
-            twitter_link = [link['href'] for link in links if 'twitter.com' in link['href']][0]
-        except:
-            twitter_link = None
-        try:
-            telegram_link = [link['href'] for link in links if 't.me' in link['href']][0]
-        except:
-            telegram_link = None
-    except:
-        pass
+    twitter_link, telegram_link = get_important_urls(url)
 
     return page_texts + document_texts, twitter_link, telegram_link
