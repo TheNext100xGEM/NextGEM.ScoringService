ebcfb5e507a0e016da8c54b4258b5e8a2e30bf78
Gergő <46539838+gergo-szabo@users.noreply.github.com>
2024-03-10 05:57:39 +0100

    stealthy selenium headless
    scrapes sites made with react

--- crawler.py
+++ crawler.py
@@ -1,16 +1,26 @@
 import requests
-import re
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium_stealth import stealth
 from bs4 import BeautifulSoup
 import fitz  # PyMuPDF
 
 
-def get_links(url: str):
+def get_soup(driver, url: str):
+    driver.get(url)
+    while driver.execute_script("return document.readyState") != "complete":
+        pass
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    return soup
+
+
+def get_links(driver, url: str, logger):
     """ Get all page and document links from a given URL """
     links = []
     documents = []
     try:
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
+        soup = get_soup(driver, url)
+        #logger.info(soup)
         for link in soup.find_all('a'):
             href = link.get('href')
             if 'http' not in href:
@@ -25,19 +35,18 @@ def get_links(url: str):
     return links, documents
 
 
-def get_page_text(url: str):
+def get_page_text(driver, url: str):
     """ Get all text fom a page """
     try:
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, 'html.parser')
+        soup = get_soup(driver, url)
         paragraphs = soup.find_all(text=True)
-    except:
+    except Exception:
         return ''
 
     texts = [p.text for p in paragraphs if len(p.text) > 3]
 
     if len(texts) > 1:
-        texts = [texts[i] for i in range(1,len(texts) - 1) if texts[i] != texts[i+1]]
+        texts = [texts[i] for i in range(1, len(texts) - 1) if texts[i] != texts[i+1]]
 
     return '\n'.join(texts)
 
@@ -58,28 +67,49 @@ def get_pdf_text(url: str):
                 pass
 
         pdf.close()  # Close the document object when done
-    except:
+    except Exception:
         return ""
     return text
 
-def get_important_urls(url: str):
+
+def get_important_urls(driver, url: str):
     try:
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
+        soup = get_soup(driver, url)
         links = soup.find_all('a', href=True)
         try:
             twitter_link = [link['href'] for link in links if 'twitter.com' in link['href']][0]
-        except:
+        except Exception:
             twitter_link = None
         try:
             telegram_link = [link['href'] for link in links if 't.me' in link['href']][0]
-        except:
+        except Exception:
             telegram_link = None
-    except:
+    except Exception:
         twitter_link = telegram_link = None
     return twitter_link, telegram_link
 
-def crawl(url: str):
+
+def crawl(url: str, logger):
+    # Start headless browser (stealthy as fuck)
+    service = ChromeService()  # currently broken, install latest: executable_path=ChromeDriverManager().install()
+
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")  # run in headless mode
+    options.add_argument('--disable-blink-features=AutomationControlled')
+    options.add_argument('--disable-popup-blocking')  # disable pop-up blocking
+    options.add_argument('--start-maximized')  # start the browser window in maximized mode
+    options.add_argument('--disable-extensions')  # disable extensions
+    options.add_argument('--no-sandbox')  # disable sandbox mode
+    options.add_argument('--disable-dev-shm-usage')  # disable shared memory usage
+
+    driver = webdriver.Chrome(service=service, options=options)
+    driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+
+    # TODO: rotate user agent if needed (https://www.zenrows.com/blog/selenium-stealth#scrape-with-stealth)
+
+    stealth(driver, languages=["en-US", "en"], vendor="Google Inc.", platform="Win32", webgl_vendor="Intel Inc.",
+            renderer="Intel Iris OpenGL Engine", fix_hairline=True)
+
     # Collect page and document links related to the input URL
     visited = set()
     document_url_list = []
@@ -90,7 +120,7 @@ def crawl(url: str):
     for current_url in level_0:
         if current_url not in visited:
             visited.add(current_url)
-            page_links, document_urls = get_links(current_url)
+            page_links, document_urls = get_links(driver, current_url, logger)
             document_url_list.extend(document_urls)
             level_1.extend([link for link in page_links if link not in visited])
 
@@ -99,13 +129,13 @@ def crawl(url: str):
             continue  # Scrape only the project technical documentation in 2 level depth
         if current_url not in visited:
             visited.add(current_url)
-            page_links, document_urls = get_links(current_url)
+            page_links, document_urls = get_links(driver, current_url, logger)
             document_url_list.extend(document_urls)
             level_2.extend([link for link in page_links if link not in visited])
 
     # Parse pages
     url_list = set(level_0 + level_1 + level_2)
-    page_texts_raw = [(i, get_page_text(i)) for i in url_list]
+    page_texts_raw = [(i, get_page_text(driver, i)) for i in url_list]
     page_texts = [f"The following text is from {i}:\n{text}" for i, text in page_texts_raw if len(text) > 0]
     page_texts = list(set(page_texts))  # deduplication
 
@@ -113,8 +143,11 @@ def crawl(url: str):
     document_texts_raw = [(i, get_pdf_text(i)) for i in document_url_list]
     document_texts = [f"The following text is from {i}:\n{text}" for i, text in document_texts_raw if len(text) > 0]
     document_texts = list(set(document_texts))  # deduplication
-    
+
     # Twitter and telegram links
-    twitter_link, telegram_link = get_important_urls(url)
+    twitter_link, telegram_link = get_important_urls(driver, url)
+
+    # Clean up headless browser
+    driver.quit()
 
     return page_texts + document_texts, twitter_link, telegram_link
